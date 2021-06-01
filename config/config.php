2c0a07d9f2867630da7a529aea6aa3711b6cceca
Moritz Walter <moritz.walter@the-systems.eu>
2021-06-01 12:19:44 +0200

    update bot

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+    "bot_token" => "",
+    "prefix" => "",
+    "presence" => array(
+        "status" => "ONLINE",
+        "text" => "mit PHP",
+        "type" => "PLAYING"
+    )
+);
